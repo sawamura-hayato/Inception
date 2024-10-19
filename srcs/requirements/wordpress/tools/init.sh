069264508abc48329f1aa5f43caabfebd13b01a4
sawamura-hayato <sunzecun@gmail.com>
2024-10-20 05:37:24 +0900

    "Update: 443ポート適用"
    実行
    `curl https://localhost:443 --insecure`
    
    ブラウザの場合はadvancedっていう左下ボタンを押して、安全じゃなくても表示するにすればwordpressのホームページが表示される

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -9,4 +9,4 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --locale=ja --allow-root
 wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_ADMIN} --dbpass=${WP_ADMIN_PASSWORD} --dbhost=${WP_DB_HOST} --allow-root
 wp core install --url=${WP_DOMAIN_NAME} --title="test Inception" --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -F
